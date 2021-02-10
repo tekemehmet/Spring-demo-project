4d441d7473ee7341ed90f0049e77a0f2b4c6df31
tekemehmet <tekemehmet@yahoo.com>
2021-02-09 23:22:11 -0500

    first commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+
+        return args->{
+            Student mehmet = new Student(
+                            "Mehmet",
+                            "tekemehmet@yahoo.com",
+                            LocalDate.of(1980, JANUARY, 9)
+
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@yahoo.com",
+                    LocalDate.of(2000, JANUARY, 19)
+            );
+
+            repository.saveAll(
+                    List.of(mehmet,alex)
+            );
+
+        };
+    }
+}
